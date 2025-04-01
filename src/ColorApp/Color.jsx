6a3f55e030c7b7dd6a1c4de014bdef5f71f496b5
Wilkins Cheng <wilkins.cheng@gmail.com>
2025-04-01 11:31:19 +1100

    added the range of colors with hex value, clipboard api, react toast

--- src/ColorApp/Color.jsx
+++ src/ColorApp/Color.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { toast } from 'react-toastify'
 
-export const Color = () => {
+export const Color = ({index, color}) => {
+    const {hex, weight} = color
+    const saveToClipboard = async () => {
+        if(navigator.clipboard){
+            try{
+                await navigator.clipboard.writeText(`#${hex}`)
+                toast.success(`Color #${hex} copied to clipboard`)
+            }catch (error)
+            {
+                toast.error('Failed to copy color to clipboard')
+            }
+        } else {
+            toast.error('Clipboard access not available')
+        }
+    }
   return (
-    <div>Color</div>
+    <div className= {index > 5 ? 'color color-light' : 'color'} style={{background: `#${hex}`}}>
+        <p className='percent-value'>{weight}%</p>
+        <p className='color-value' onClick={saveToClipboard} >#{hex}</p>
+    </div>
   )
 }
